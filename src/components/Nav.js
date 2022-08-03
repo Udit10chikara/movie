e402f2f0b3b0d5f1a5da34c7e7b77ef2d7548eee
Udit10chikara <110520491+Udit10chikara@users.noreply.github.com>
2022-08-03 14:45:01 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ export default function Nav() {
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
-        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+        <Link className="nav-link" to="/movie">Home <span className="sr-only">(current)</span></Link>
       </li>
       <li className="nav-item">
         <Link className="nav-link" to="/movie">Movie</Link>
